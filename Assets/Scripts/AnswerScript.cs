6c8d14dce1366a174ac72b5962875cb9f4ef8760
KahHin <chiahkahhin@hotmail.com>
2021-10-05 13:54:58 +0800

    Enhance animation call function at script

--- Assets/Scripts/AnswerScript.cs
+++ Assets/Scripts/AnswerScript.cs
@@ -19,5 +19,16 @@ public class AnswerScript : MonoBehaviour
             Debug.Log("Wrong Answer");
             mcq.correct("Wrong Answer");
         }
+        test();
+    }
+
+    public void test()
+    {
+        Debug.Log(4);
+        GameObject.Find("A_button").GetComponent<Animator>().Play("AButtonLeft");
+        GameObject.Find("B_button").GetComponent<Animator>().Play("BButtonRight");
+        GameObject.Find("C_button").GetComponent<Animator>().Play("CButtonLeft");
+        GameObject.Find("D_button").GetComponent<Animator>().Play("DButtonRight");
+        GameObject.Find("MessagePanel").GetComponent<Animator>().Play("MessagePanelEnlarge");
     }
 }
