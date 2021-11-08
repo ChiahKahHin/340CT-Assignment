d42093caf8b3f5be70a911fb0c2b43a8b0e1aec1
HawLiang <55545353+LSMicah@users.noreply.github.com>
2021-11-08 21:05:46 +0800

    add pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenuUI;
+
+    public void Resume()
+    {
+        Debug.Log("Resume Game...");
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        Debug.Log("Game Paused...");
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void loadMenu()
+    {
+        Debug.Log("Back to Main Menu...");
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+}
